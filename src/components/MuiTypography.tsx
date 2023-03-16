fec2e880d4084893801ffd10c8e4eac3582ebb0e
anuragroopchandani <anuragroopchandani@gmail.com>
2023-03-16 12:33:28 +0530

    Initial Commit

---
+++ src/components/MuiTypography.tsx
@@ -0,0 +1,22 @@
+import { Typography } from '@mui/material';
+
+export const MuiTypography = () => {
+    return (
+        <div>
+            <Typography variant='h1'>Heading 1</Typography>
+            <Typography variant='h2'>Heading 2</Typography>
+            <Typography variant='h3'>Heading 3</Typography>
+            <Typography variant='h4'>Heading 4</Typography>
+            <Typography variant='h5'>Heading 5</Typography>
+            <Typography variant='h6'>Heading 6</Typography>
+
+            <Typography variant='subtitle1'>Subtitle 1</Typography>
+            <Typography variant='subtitle2'>Subtitle 2</Typography>
+
+
+            <Typography variant='body1'>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eos velit dolor ab amet at voluptatibus blanditiis fugiat magnam eveniet provident?</Typography>
+
+            <Typography variant='body2'>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eos velit dolor ab amet at voluptatibus blanditiis fugiat magnam eveniet provident?</Typography>
+        </div>
+    )
+}
